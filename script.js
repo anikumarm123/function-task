4e2d1377374bc1e38bb054def035788857d55697
muthukumar <anikumar2825@gmail.com>
2022-06-08 14:45:13 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,94 @@
+//Addition//
+// function addition (a,b){
+//  var a = 10, b = 20;   
+//  add = a + b;
+//  console.log(add);
+// }
+// addition()
+
+
+// Subtrction //
+// function Subtrction (a,b){
+//     var a = 10 ,b = 5;
+//     sub = a - b;
+//     console.log(sub);
+// }
+// Subtrction()
+
+
+//multiplication//
+// function multiplication(){
+//     a = 10 ,b = 20;
+//     mul = a * b;
+//     return mul;
+// }
+// result = multiplication();
+// console.log(result);
+
+//divition//
+// function divition(){
+//     a = 10 ,b = 5;
+//     div = a / b;
+//     return div
+// }
+// result = divition();
+// console.log(result);
+
+
+//reverse of number//
+
+// function reverse_number(){
+//     var n = 10;
+//     for(i = 0; i <= n; i++){
+//         b = n - i;
+//         console.log(b);
+//     }
+// }
+// reverse_number()
+
+
+
+
+//Even or odd //
+// function Even(){
+//     var n = parseInt(prompt("Enter The Number"));
+//     if (n % 2 == 0){
+//         console.log(n+" is Even Number");
+//     }
+//     else{
+//         console.log(n + " is Odd Number");
+//     }
+//     return n
+// }
+// result = Even()
+// Even()
+
+// Grade based on mark //
+// function Grade(){
+//     var mark =90;
+//     if(mark >= 90){
+//         console.log("A grade");
+//     }
+//     else if (mark >= 80){
+//         console.log("B grade");
+//     }
+//     else if (mark >= 50){
+//         console.log("c grade");
+//     }
+//     else {
+//         console.log("fail");
+//     }
+//     return mark
+// }
+// Grade()
+
+
+
+
+
+
+
+
+
+
+
